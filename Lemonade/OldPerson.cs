eae9f76850fc8856da78d29588e9afc93dea57f8
Ryan Kunz <ryanmkunz@gmail.com>
2019-04-03 13:24:03 -0500

    Added override methods to child classes of Customer. Each likes a different recipe

--- Lemonade/OldPerson.cs
+++ Lemonade/OldPerson.cs
@@ -7,5 +7,22 @@ namespace Lemonade
 {
     public class OldPerson : Customer
     {
+        public OldPerson()
+        {
+
+        }
+
+        public virtual bool IsBuying(int demand, Player player)
+        {
+            //OldPerson likes recipes with less sugar 
+            if (player.SugarPerPitcher < 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
