b2935988d88001a536ab73350fc89c4251938aaa
Ryan Kunz <ryanmkunz@gmail.com>
2019-04-03 11:41:37 -0500

    Added child classes to Customer (child, adult, old person) with different recipe preferences

--- Lemonade/Day.cs
+++ Lemonade/Day.cs
@@ -12,19 +12,36 @@ namespace Lemonade
         public double Revenue;
         public Weather weather;
         public Customer customer;
+        private Random random;
+        public int CustomerType;
 
         public Day()
         {
             CupsSold = 0;
-            TotalCustomers = 300;
+            TotalCustomers = 1000;
             weather = new Weather();
+            random = new Random();
         }
 
         public void GetDailySales(int demand)
         {
             for (int i = 0; i < TotalCustomers; i++)
             {
-                customer = new Customer();
+                CustomerType = random.Next(1, 4);
+                switch (CustomerType)
+                {
+                    case 1:
+                        customer = new Child();
+                        break;
+                    case 2:
+                        customer = new Adult();
+                        break;
+                    case 3:
+                        customer = new OldPerson();
+                        break;
+                    default:
+                        break;
+                }
                 if (customer.IsBuying(demand))
                 {
                     CupsSold++;
